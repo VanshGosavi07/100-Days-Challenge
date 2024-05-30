f537e14a9479105a2222703fb9c826830190ab75
VanshGosavi07 <vanshgosavi7@gmail.com>
2024-05-30 22:08:49 +0530

    Snake Game Part 2 is completed

--- Day 20 Snake Game/snake.py
+++ Day 20 Snake Game/snake.py
@@ -1,7 +1,6 @@
 from turtle import Turtle
 
 POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
-TURTLES = []
 UP = 90
 DOWN = 270
 LEFT = 180
@@ -10,19 +9,17 @@ RIGHT = 0
 
 class Snake:
     def __init__(self):
+        self.turtles=[]
         for position in POSITIONS:
-            obj = Turtle('square')
-            obj.color('white')
-            obj.penup()
-            obj.goto(position)
-            TURTLES.append(obj)
-        self.head = TURTLES[0]
+            self.add_segment(position)
+
+        self.head = self.turtles[0]
 
     def move(self):
-        for i in range(len(TURTLES) - 1, 0, -1):
-            x = TURTLES[i - 1].xcor()
-            y = TURTLES[i - 1].ycor()
-            TURTLES[i].goto(x, y)
+        for i in range(len(self.turtles) - 1, 0, -1):
+            x = self.turtles[i - 1].xcor()
+            y = self.turtles[i - 1].ycor()
+            self.turtles[i].goto(x, y)
         self.head.forward(20)
 
     def left(self):
@@ -30,7 +27,7 @@ class Snake:
             self.head.setheading(LEFT)
 
     def right(self):
-        if self.head.heading()!=LEFT:
+        if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
 
     def up(self):
@@ -40,3 +37,14 @@ class Snake:
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
+
+    def add_segment(self, position):
+        obj = Turtle('square')
+        obj.width(40)
+        obj.color('white')
+        obj.penup()
+        obj.goto(position)
+        self.turtles.append(obj)
+
+    def extend(self):
+        self.add_segment(self.turtles[-1].position())
